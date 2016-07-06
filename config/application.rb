23de5651d0c4e46fac6b01c30cb61ba86e35675b
Scott Pierce <scott.pierce@centro.net>
2016-07-06 10:56:33 -0500

    Filled out example code.

--- config/application.rb
+++ config/application.rb
@@ -7,20 +7,26 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module RackAttack
+
+  class HijackHandler
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if env['REQUEST_PATH'].starts_with?('/hijack')
+        [200, {}, ["Rack attack owns you! (And we're fast, too)"]]
+      else
+        # Pass it down the chain to Rails default middlewares
+        @app.call(env)
+      end
+    end
+  end
+
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, 'RackAttack::HijackHandler'
   end
 end
